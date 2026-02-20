51be120c24f8928f3f9a4b12834fbf520bcd882e
Murali Krishna Palla <pmuralikrishna520@gmail.com>
2026-02-20 10:15:56 +0530

    connected frontend to live backend

--- expense-tracker-frontend/src/api/api.js
+++ expense-tracker-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:9000/api"
+  baseURL: "https://expense-tracker-1-gdke.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
