260ed49919b6cc28a2d393584a909405478d6dd0
Murali Krishna Palla <pmuralikrishna520@gmail.com>
2026-02-23 15:17:09 +0530

    Update CORS origin to new Vercel deployment URL

--- expense-tracker-backend/server.js
+++ expense-tracker-backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 // =====================
 app.use(cors({
-    origin: "https://expense-tracker-7zjk93fah-muralis-projects-53a36327.vercel.app",
+    origin: "https://expense-tracker-seven-nu-65.vercel.app",
     credentials: true
   }));
 app.use(express.json());
